fa693b5f96766c64965af2171802d2de553516ba
seowting <104329782+seowting@users.noreply.github.com>
2022-11-30 15:36:28 +0800

    Add files via upload

---
+++ JavaDoc/type-search-index.js
@@ -0,0 +1,5 @@
+typeSearchIndex = [{"l": "All Classes", "u": "allclasses-index.html"}, {"p": "my.uum", "l": "App"}, {
+    "p": "my.uum",
+    "l": "Class_A"
+}, {"p": "my.uum", "l": "Class_B"}, {"p": "my.uum", "l": "Class_C"}];
+updateSearchResults();
